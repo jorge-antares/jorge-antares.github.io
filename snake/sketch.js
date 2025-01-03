89e72ed7fb3eb25332cd8b94454efd2073ba58ec
Jorge A. Garcia <ja4garci@uwaterloo.ca>
2025-01-03 17:20:46 -0500

    style

--- snake/sketch.js
+++ snake/sketch.js
@@ -4,20 +4,20 @@ var biteLoc = [];
 var alive = true;
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(700, 700);
   background(255);
   s = new Snake();
-  createBite(ds,s);
+  createBite(ds, s);
 }
 
 function draw() {
   background(255);
   frameRate(8);
   alive = s.die(ds);
-  
+
   if (alive) {
     colorBite(ds);
-    s.eat(biteLoc,ds);
+    s.eat(biteLoc, ds);
     s.move(ds);
     s.show(ds);
   } else {
@@ -25,47 +25,47 @@ function draw() {
     background(0);
     fill(255, 0, 0);
     textSize(30);
-    text('Game over',width/3,height/3);
+    text('Game over', width / 3, height / 3);
     fill(255);
     textSize(20);
-    text('Your score is '+ w +'!',width*0.35,height*0.6);
+    text('Your score is ' + w + '!', width * 0.35, height * 0.6);
     noLoop();
   }
   fill(0);
-  rect(0,0,width,ds);
-  rect(0,height,width,-ds);
-  rect(0,0,ds,height);
-  rect(width,0,-ds,height);
+  rect(0, 0, width, ds);
+  rect(0, height, width, -ds);
+  rect(0, 0, ds, height);
+  rect(width, 0, -ds, height);
 }
 
 function keyPressed() {
   if ((keyCode === LEFT_ARROW) && (s.heading != 1)) {
-    s.dir(-1,0);
+    s.dir(-1, 0);
   } else if ((keyCode === RIGHT_ARROW) && (s.heading != -1)) {
-    s.dir(1,0);
+    s.dir(1, 0);
   } else if ((keyCode === UP_ARROW) && (s.heading != 2)) {
-    s.dir(0,-1);
+    s.dir(0, -1);
   } else if ((keyCode === DOWN_ARROW) && (s.heading != -2)) {
-    s.dir(0,1);
+    s.dir(0, 1);
   }
 }
 
 
 function colorBite(ds) {
-  fill(36,155,144);
-  rect(biteLoc[0],biteLoc[1],ds,ds);
+  fill(36, 155, 144);
+  rect(biteLoc[0], biteLoc[1], ds, ds);
 }
 
 
-function createBite(ds,snake) {
-  biteLoc[0] = ds * floor(random(1,width/ds-1));
-  biteLoc[1] = ds * floor(random(1,height/ds-1));
+function createBite(ds, snake) {
+  biteLoc[0] = ds * floor(random(1, width / ds - 1));
+  biteLoc[1] = ds * floor(random(1, height / ds - 1));
 
   for (let i = 0; i < snake.location[0].length; i++) {
     if ((biteLoc[0] == snake.location[0][i]) &&
-        (biteLoc[0] == snake.location[0][i])) {
-      biteLoc[0] = ds * floor(random(1,width/ds-1));
-      biteLoc[1] = ds * floor(random(1,height/ds-1));
+      (biteLoc[0] == snake.location[0][i])) {
+      biteLoc[0] = ds * floor(random(1, width / ds - 1));
+      biteLoc[1] = ds * floor(random(1, height / ds - 1));
       i = 0;
     }
   }
